48ccdaaa1ec757035ade1816a6895e21a43e307b
Jenn Purnell <jennpurnell@gmail.com>
2017-01-28 15:42:01 -0800

    My first commit for C# class project

---
+++ My First Bank/Account.cs
@@ -0,0 +1,71 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_First_Bank
+{
+    /// <summary>
+    /// This class defines the account for a bank.
+    /// This is the blueprint
+    /// </summary>
+    class Account
+    {
+        #region Statics
+        private static int lastAccountNumber = 0;
+        #endregion
+
+        
+        
+        
+        #region Properties
+        public int AccountNumber { get; private set; }
+
+        public string EmailAddress { get; set; }
+
+        public decimal Balance { get; private set; }
+        #endregion
+
+        #region Methods
+        
+        //constructors
+       
+        public Account() : this("", 0.0M)
+        {
+            //lastAccountNumber = lastAccountNumber + 1;
+            //thisAccountNumber = lastAccountNumber += 1;
+            //this.AccountNumber = ++lastAccountNumber;
+        }
+
+        public Account(string emailAddress) : this(emailAddress, 0.0M)
+        {
+            //this.AccountNumber = ++lastAccountNumber;
+            //this.EmailAddress = emailAddress;
+        }
+
+        public Account(string emailAddress, decimal balance)
+        {
+            this.AccountNumber = ++lastAccountNumber;
+            this.EmailAddress = emailAddress;
+            this.Balance = balance;
+        }
+
+        // methods that are not constructors
+        public decimal Deposit(decimal amount)
+        {
+            //Balance = Balance + amount;
+            Balance += amount;
+            return Balance;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+        #endregion
+    }
+}
+
---
+++ My First Bank/AccountProgram.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_First_Bank
+{
+    class AccountProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******Welcome to my bank!******");
+            while (true)
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Create an account");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("Please select one of the options from above:");
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "0":
+                        return;
+                    case "1":
+                        Console.WriteLine("Please enter your email address:");
+                        var emailAddress = Console.ReadLine();
+                        var myAccount = new Account(emailAddress);
+                        Console.WriteLine($"The balance in my account - {myAccount.AccountNumber} is {myAccount.Balance:C} ");
+                        break;
+                    case "2":
+
+                        break;
+                    case "3":
+
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, option not available");
+                        break;
+
+                }
+            }
+            ////create an account object/instance
+            //var myAccount = new Account();
+            //myAccount.EmailAddress = "test@test.com";
+            //myAccount.Deposit(200.50M);
+            //Console.WriteLine($"The balance in my account - {myAccount.AccountNumber} is {myAccount.Balance:C} ");
+
+            //var myAccount2 = new Account("test2@test.com");
+            ////myAccount2.EmailAddress = "test@test.com";
+            //myAccount2.Deposit(200.50M);
+            //Console.WriteLine($"The balance in my account - {myAccount2.AccountNumber} is {myAccount2.Balance:C} ");
+
+            //var myAccount3 = new Account("test2@test.com", 200.50M);
+            ////myAccount2.EmailAddress = "test@test.com";
+            ////myAccount3.Deposit(200.50M);
+            //Console.WriteLine($"The balance in my account - {myAccount3.AccountNumber} is {myAccount3.Balance:C} ");
+
+        }
+    }
+}
